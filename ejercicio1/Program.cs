e0b399191f198725ac7eedf8b2dff7db8df13df7
noeking23 <noecarlosmamaniespinoza@gmail.com>
2023-10-25 09:29:27 -0400

    Programas

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Comparacion
+{
+    public static void DeterminarElMayor()
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        if (numero1 > numero2)
+        {
+            Console.WriteLine($"El número mayor es: {numero1}");
+        }
+        else if (numero2 > numero1)
+        {
+            Console.WriteLine($"El número mayor es: {numero2}");
+        }
+        else
+        {
+            Console.WriteLine("Ambos números son iguales.");
+        }
+    }
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Evaluacion
+{
+    public static void DeterminarSigno()
+    {
+        Console.WriteLine("Ingrese un número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El número es positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El número es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("El número es cero.");
+        }
+    }
+}
